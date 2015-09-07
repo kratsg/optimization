f282e2606252e7a1d82c97eaa3e05c01f981f981
Giordon Stark <kratsg@gmail.com>
2015-09-07 11:59:10 -0500

    details for running njet Max

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,8 @@
 # first, run to produce baseline selections
 
 python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/*_0L_a/fetch/data-optimizationTree/*.root --supercuts=supercuts_baseline.json -o baselineCuts --numpy -v -b
+
+# njet was maxed at 10, but this overlaps with another group. How bad is it if we bump this down to a max of 7?
+
+python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/*_0L_a/fetch/data-optimizationTree/*.root --supercuts=supercuts_njetMax.json -o njetMaxCuts --numpy -v -b
+
