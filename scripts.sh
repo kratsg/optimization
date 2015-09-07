8a3a92676106fb59c66bc1d85df77504077b6b2f
Giordon Stark <kratsg@gmail.com>
2015-09-07 13:08:33 -0500

    A full workflow

--- scripts.sh
+++ scripts.sh
@@ -6,3 +6,17 @@ python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/*_0L_a/fet
 
 python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/*_0L_a/fetch/data-optimizationTree/*.root --supercuts=supercuts_njetMax.json -o njetMaxCuts --numpy -v -b
 
+
+# next, we want to compute significances for it all
+python optimize.py optimize --signal 37* --bkgd 4* --searchDirectory=njetMaxCuts -b --o=njetMaxSignificances --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=0.5 --lumi=1
+
+python optimize.py optimize --signal 37* --bkgd 4* --searchDirectory=njetMaxCuts -b --o=njetMaxSignificances --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=0.5 --lumi=1
+
+# next, find all optimal hashes by running
+python write_all_optimal_cuts.py
+# after editing the top few lines for appropriate directories
+
+# finally, make plots
+python graph-grid.py --lumi 1 --outfile baseline --sigdir baselineSignificances --cutdir baselineCuts
+
+python graph-grid.py --lumi 1 --outfile njetMax --sigdir njetMaxSignificances --cutdir njetMaxCuts
