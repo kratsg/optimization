57b8cab0b9ccc404195056584bf344391f13b0bd
Giordon Stark <kratsg@gmail.com>
2015-09-07 17:48:37 -0500

    need to be more specific on which ttbar files to use...

--- scripts.sh
+++ scripts.sh
@@ -1,10 +1,10 @@
 # first, run to produce baseline selections
 
-python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/*_0L_a/fetch/data-optimizationTree/*.root --supercuts=supercuts_baseline.json -o baselineCuts --numpy -v -b
+python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/Gtt_0L_a/fetch/data-optimizationTree/*.root ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/ttbar*_0L_a/fetch/data-optimizationTree/*410000*r6765_r6282*.root ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/ttbar*_0L_a/fetch/data-optimizationTree/*407012*r6765_r6282*p2411*.root --supercuts=supercuts_baseline.json -o baselineCuts --numpy -v -b
 
 # njet was maxed at 10, but this overlaps with another group. How bad is it if we bump this down to a max of 7?
 
-python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/*_0L_a/fetch/data-optimizationTree/*.root --supercuts=supercuts_njetMax.json -o njetMaxCuts --numpy -v -b
+python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/Gtt_0L_a/fetch/data-optimizationTree/*.root ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/ttbar*_0L_a/fetch/data-optimizationTree/*410000*r6765_r6282*.root ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/ttbar*_0L_a/fetch/data-optimizationTree/*407012*r6765_r6282*p2411*.root --supercuts=supercuts_njetMax.json -o njetMaxCuts --numpy -v -b
 
 
 # next, we want to compute significances for it all
