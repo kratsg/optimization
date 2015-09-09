1c2a28d6f25cdc27acf905777cab0e5edeafe7b4
Giordon Stark <kratsg@gmail.com>
2015-09-09 18:53:32 -0500

    Now plots optimal SR for each lumi correctly. Need to flesh out the options and shit.

--- find_optimal_signal_region.py
+++ find_optimal_signal_region.py
@@ -12,22 +12,27 @@ parser = argparse.ArgumentParser(description='Author: A. Cukierman, G. Stark. v.
                                  formatter_class=lambda prog: CustomFormatter(prog, max_help_position=30))
 parser.add_argument('--lumi', required=False, type=int, dest='lumi', metavar='<L>', help='luminosity to use', default=1)
 parser.add_argument('-o', '--output', required=False, type=str, dest='output', metavar='', help='basename to use for output filename', default='output')
+parser.add_argument('-d', '--output-dir', required=False, type=str, dest='output_dir', metavar='', help='directory to put it in', default='plots')
 
 # parse the arguments, throw errors if missing any
 args = parser.parse_args()
 
 import ROOT
+ROOT.gROOT.SetBatch(True)
 import csv
 import glob
 import re
 import json
 from collections import defaultdict
 
+from rootpy.plotting.style import set_style, get_style
+set_style('ATLAS')
+
 # given a DID, we get the mass points, translates to a box on the graph for us
 with open('mass_windows.txt', 'r') as f:
   reader = csv.reader(f, delimiter='\t')
   m = list(reader)
-mdict = {l[0]: [l[1],l[2],l[3]] for l in m}
+mdict = {l[0]: [int(l[1]),int(l[2]),int(l[3])] for l in m}
 del m
 
 # start up a dictionary to hold all information
@@ -50,3 +55,63 @@ for did, vals in significances.iteritems():
   winners[max(vals.iteritems(), key=operator.itemgetter(1))[0]] += 1
 
 print winners
+
+ROOT.gStyle.SetPalette(1)
+c = ROOT.TCanvas("c", "", 0, 0, 800, 600)
+c.SetRightMargin(0.16)
+c.SetTopMargin(0.07)
+
+h = ROOT.TH2F("grid", ";m_{#tilde{g}} [GeV]; m_{#tilde{#chi}^{0}_{1}} [GeV];%s" % "Optimal Signal Region", 12, 800, 2000, 13, 0, 1300)
+for did, vals in significances.iteritems():
+  winningSR = max(vals.iteritems(), key=operator.itemgetter(1))[0]
+  mgluino, mstop, mlsp = mdict[did]
+  if mstop != 5000: continue
+
+  # now, let's find the bin to fill
+  b = h.FindFixBin(mgluino, mlsp)
+  xx = yy = zz = ROOT.Long(0)
+  h.GetBinXYZ(b, xx, yy, zz)
+  if xx != 0 or yy != 0 or zz != 0:
+    print "bin was already set?\n\txx: {0}\n\tyy: {1}\n\tzz: {2}".format(xx, yy, zz)
+    print "mgluino: {0}\tmlsp: {1}".format(mgluino, mlsp)
+    print "new value: {0}".format(winningSR)
+    print "-"*20
+  h.SetBinContent(b, winningSR)
+
+# now draw it
+h.SetMarkerSize(1.5)
+h.SetMarkerColor(ROOT.kWhite)
+ROOT.gStyle.SetPalette(51)
+ROOT.gStyle.SetPaintTextFormat("1.0f")
+h.Draw("TEXT COLZ")
+
+# fix the ZAxis
+h.GetZaxis().SetRangeUser(1, 4)
+h.SetContour(4)
+h.GetZaxis().SetNdivisions(3, False)
+
+txt = ROOT.TLatex()
+txt.SetNDC()
+txt.DrawText(0.32,0.87,"Internal")
+txt.DrawText(0.2,0.82,"Simulation")
+txt.SetTextSize(0.030)
+txt.DrawLatex(0.16,0.95,"#tilde{g}-#tilde{g} production, #tilde{g} #rightarrow t #bar{t} + #tilde{#chi}^{0}_{1}")
+txt.DrawLatex(0.62,0.95,"L_{int} = %d fb^{-1}, #sqrt{s} = 13 TeV"% args.lumi)
+txt.SetTextFont(72)
+txt.SetTextSize(0.05)
+txt.DrawText(0.2,0.87,"ATLAS")
+txt.SetTextFont(12)
+txt.SetTextAngle(38)
+txt.SetTextSize(0.02)
+txt.DrawText(0.33,0.63,"Kinematically Forbidden")
+
+topmass = 173.34
+
+l=ROOT.TLine(1000,1000,2000,2000)
+l.SetLineStyle(2)
+l.DrawLine(800,800-2*topmass,1300+2*topmass,1300)
+
+savefilename = '{0}_maxSR{1}'.format(os.path.join(args.output_dir, args.output), args.lumi)
+c.SaveAs(savefilename + ".pdf")
+print "Saving file " + savefilename
+c.Clear()
