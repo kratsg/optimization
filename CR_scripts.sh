c949d5150329c3ddd5e68f6c4840aae647b32cab
Giordon Stark <kratsg@gmail.com>
2015-09-17 11:04:49 -0500

    remove folders before making them, so we don't manually remove ourselves

--- CR_scripts.sh
+++ CR_scripts.sh
@@ -4,14 +4,23 @@ do
   supercutsLocation="supercuts/CR-${i}.json"
   cutsLocation="CR${i}Cuts"
 
-  python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/Gtt_1L/fetch/data-optimizationTree/*.root ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/ttbar*_1L/fetch/data-optimizationTree/*410000*r6765_r6282*.root ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V1/ttbar*_1L/fetch/data-optimizationTree/*407012*r6765_r6282*p2411*.root --supercuts $supercutsLocation -o $cutsLocation --numpy -v -b
+  rm -rf $cutsLocation
+
+  python optimize.py cut ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V2/Gtt_1L/fetch/data-optimizationTree/*.root ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V2/ttbar*_1L/fetch/data-optimizationTree/*410000*r6765_r6282*.root ~/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V2/ttbar*_1L/fetch/data-optimizationTree/*407012*r6765_r6282*p2411*.root --supercuts $supercutsLocation -o $cutsLocation --numpy -v -b
 
   for lumi in 1 2 4 10
   do
+
     significancesLocation="CR${i}Significances_${lumi}"
+
+    rm -rf $significancesLocation
+
     python optimize.py optimize --signal 37* --bkgd 4* --searchDirectory $cutsLocation -b --o $significancesLocation --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=0.5 --lumi $lumi
 
     outputHashLocation="outputHash_CR${i}_${lumi}"
+
+    rm -rf $outputHashLocation
+
     python write_all_optimal_cuts.py --supercuts $supercutsLocation --significances $significancesLocation -o $outputHashLocation
 
     outputFilePlots="CR${i}_${lumi}"
