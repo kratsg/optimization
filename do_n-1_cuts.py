fa3c26d1de28b1633e7bff3f22b396444ddbe158
Giordon Stark <kratsg@gmail.com>
2015-09-23 18:08:20 -0400

    We want to make histograms of the n-1 stuff. Not cuts.. DUHHHHHH

--- do_n-1_cuts.py
+++ do_n-1_cuts.py
@@ -18,7 +18,6 @@ parser = argparse.ArgumentParser(description='Author: G. Stark. v.{0}'.format(__
                                  formatter_class=lambda prog: CustomFormatter(prog, max_help_position=30))
 parser.add_argument('files', type=str, nargs='+', metavar='<file.root>', help='ROOT files containing the optimization ntuples')
 parser.add_argument('--supercuts', required=True, type=str, dest='supercuts', metavar='<file.json>', help='json dict of supercuts to generate optimization cuts to apply')
-parser.add_argument('--weightsFile', type=str, required=False, dest='weightsFile', metavar='<weights file>', help='yml file containing weights by DID', default='weights.yml')
 parser.add_argument('--output', required=False, type=str, dest='output', metavar='', help='name of directory to keep all generated outputs', default='n-1')
 
 parser.add_argument('--tree', type=str, required=False, dest='tree_name', metavar='<tree name>', help='name of the tree containing the ntuples', default='oTree')
@@ -30,11 +29,14 @@ args = parser.parse_args()
 import json
 from itertools import combinations
 import operator
+import optimize
+import ROOT
 
 supercuts = json.load(file(args.supercuts, 'r'))
 
 differences = []
 level = 0
+c = ROOT.TCanvas("canvas", "canvas", 500, 500)
 for subercuts in combinations(supercuts, len(supercuts)-1):
   level += 1
   # hold the differences and create a text file with them later for reference
@@ -45,6 +47,30 @@ for subercuts in combinations(supercuts, len(supercuts)-1):
   with open(subercutsFile, 'w+') as f:
     f.write(json.dumps(subercuts, sort_keys=True, indent=4))
 
-  # build arguments for do_cuts
-  do_cuts_call = ['./optimize.py', 'cut'] + args.files + ['--supercuts', subercutsFile] + ['-o', os.path.join(args.output, str(level))] + ['--numpy', '-v', '-b']
-  subprocess.call(do_cuts_call)
+  # get the tree
+  tree = optimize.get_ttree(args.tree_name, args.files, args.eventWeightBranch)
+
+  # get the selection we apply to draw it
+  selection = optimize.cuts_to_selection(subercuts)
+  # get the branch we need to draw
+  selection_string = differences[-1]['selections']
+
+  branchesSpecified = set(optimize.selection_to_branches(selection_string, tree))
+  # get actual list of branches in the file
+  availableBranches = optimize.tree_get_branches(tree, args.eventWeightBranch)
+  # remove anything that doesn't exist
+  branchesToUse = [branch for branch in branchesSpecified if branch in availableBranches]
+
+  # more than one branch, we skip and move to the next
+  if len(branchesToUse) != 1:
+    print("Warning: selection has multiple branches.\n\tSelection: {0}".format(selection_string))
+    level -= 1
+    del differences[-1]
+    continue
+
+  # draw with selection and branch
+  tree.Draw(branchesToUse[0], '{0:s}*{1:s}'.format(args.eventWeightBranch, selection))
+
+  # write to file
+  c.SaveAs('{0}_{1}.pdf'.format(os.path.join(args.output, str(level)), branchesToUse[0]))
+  c.Clear()
