efc853a23937376a938978823d837685df4af98e
Giordon Stark <kratsg@gmail.com>
2015-10-09 07:53:22 -0500

    bump to V5

--- SR_scripts.sh
+++ SR_scripts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gttFiles=${HOME}/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V4/Gtt_0L_b/fetch/data-optimizationTree/*.root
-ttbarIncFiles=${HOME}/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V4/ttbar*_0L_b/fetch/data-optimizationTree/*410000*r6765_r6282*.root
-ttbarExcFiles=${HOME}/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V4/ttbar*_0L_b/fetch/data-optimizationTree/*407012*r6765_r6282*p2411*.root
+gttFiles=${HOME}/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V5/Gtt_0L_b/fetch/data-optimizationTree/*.root
+ttbarIncFiles=${HOME}/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V5/ttbar*_0L_b/fetch/data-optimizationTree/*410000*r6765_r6282*.root
+ttbarExcFiles=${HOME}/Dropbox/TheAccountant_dataFiles/TA07_MBJ10V5/ttbar*_0L_b/fetch/data-optimizationTree/*407012*r6765_r6282*p2411*.root
 
 baseDir="SR"
 rm -rf $baseDir
