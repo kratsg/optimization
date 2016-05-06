813ee011e871c88b2ed1d4d04f897d4342bfc32b
Giordon Stark <kratsg@gmail.com>
2016-05-05 23:15:34 -0500

    add initial stuff for gui

---
+++ gui/mainTest.rb
@@ -0,0 +1,41 @@
+require 'shoes'
+
+Shoes.setup do
+end
+
+require 'open3'
+
+Shoes.app width: 800, title: "Optimization" do
+    background "#000"
+    stack do
+        background "#000"
+        title "Optimization.. for the rest of us", :align => 'center', stroke: "#FFF"
+    end
+    stack width: 200, height: 40, margin_left: 50 do
+        background "#DFA"
+        para "Step 1"
+    end
+    stack width: -200 do
+        background "#DF0"
+        flow do
+            button "Pick your supercuts file" do
+                @configurations_file.text = ask_open_file
+            end
+            @configurations_file = para
+        end
+    end
+    stack width: 200, height: 400, margin_left: 50 do
+        background "#D0A"
+        para "Step 2"
+    end
+    stack width: -200 do
+        background "#D00"
+        subtitle "Foo"
+        button do
+            stdout, stderr, status = Open3.capture3("python ../optimize.py -h")
+            @log.text = stderr
+        end
+        @log = para
+    end
+    para "Hello World", stroke: "#00FFFF"
+end
