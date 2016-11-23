2ff53b87beb680c4695e4291043b99966ed0369b
Giordon Stark <kratsg@gmail.com>
2016-11-22 20:21:13 -0600

    move parser into main, so we can import file normally as well

--- graph-grid.py
+++ graph-grid.py
@@ -1,39 +1,6 @@
-import argparse
-import subprocess
 import os
-import utils
 import csv
 import json
-
-class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter):
-  pass
-
-__version__ = subprocess.check_output(["git", "describe", "--always"], cwd=os.path.dirname(os.path.realpath(__file__))).strip()
-__short_hash__ = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd=os.path.dirname(os.path.realpath(__file__))).strip()
-
-parser = argparse.ArgumentParser(description='Author: A. Cukierman, G. Stark. v.{0}'.format(__version__),
-                                 formatter_class=lambda prog: CustomFormatter(prog, max_help_position=30))
-parser.add_argument('--summary', type=str, required=True, help='Summary json')
-parser.add_argument('--lumi', type=float, required=False, help='Luminosity to write on plot [ifb]', default=35)
-parser.add_argument('--text-file', type=str, required=False, help='text csv file', default=None)
-parser.add_argument('--out-directory', type=str, required=False, help='output directory', default='plots')
-parser.add_argument('-o', '--output', type=str, required=False, help='Name to put in output filenames', default='output')
-parser.add_argument('--g-min', type=float, required=False, help='Minimum gluino mass', default=800)
-parser.add_argument('--g-max', type=float, required=False, help='Maximum gluino mass', default=2500)
-parser.add_argument('--l-min', type=float, required=False, help='Minimum LSP mass', default=0)
-parser.add_argument('--l-max', type=float, required=False, help='Maximum LSP mass', default=1500)
-parser.add_argument('--bin-size', type=float, required=False, help='Size of bins to use', default=100)
-parser.add_argument('--x-dim', type=float, required=False, help='x-dimension of figure', default=800)
-parser.add_argument('--y-dim', type=float, required=False, help='y-dimension of figure', default=600)
-parser.add_argument('--top-mass', type=float, required=False, help='Mass of top quark [GeV]. Mainly meant to draw exclusion line.', default=173.34)
-parser.add_argument('--do-run1', action='store_true', help='Add Run-1 line to graph')
-parser.add_argument('--run1-color', type=int, required=False, help='Color of Run-1 line', default=46)
-parser.add_argument('--run1-excl', type=str, required=False, help='CSV file containing Run-1 exclusion points', default='run1_limit.csv')
-parser.add_argument('--run1-1sigma', type=str, required=False, help='CSV file containing Run-1 exclusion (+1 sigma) points', default='run1_limit_1sigma.csv')
-parser.add_argument('-b', '--batch', dest='batch_mode', action='store_true', help='Enable batch mode for ROOT.')
-
-# parse the arguments, throw errors if missing any
-args = parser.parse_args()
 
 def nbinsx(args):
     return int((args.g_max - args.g_min) / args.bin_size)
@@ -162,10 +129,41 @@ def exclusion():
   return p
 
 if __name__ == '__main__':
+  import argparse
+  import subprocess
+
+  class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    pass
+
+  __version__ = subprocess.check_output(["git", "describe", "--always"], cwd=os.path.dirname(os.path.realpath(__file__))).strip()
+  __short_hash__ = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd=os.path.dirname(os.path.realpath(__file__))).strip()
+
+  parser = argparse.ArgumentParser(description='Author: A. Cukierman, G. Stark. v.{0}'.format(__version__),
+                                   formatter_class=lambda prog: CustomFormatter(prog, max_help_position=30))
+  parser.add_argument('--summary', type=str, required=True, help='Summary json')
+  parser.add_argument('--lumi', type=float, required=False, help='Luminosity to write on plot [ifb]', default=35)
+  parser.add_argument('--text-file', type=str, required=False, help='text csv file', default=None)
+  parser.add_argument('--out-directory', type=str, required=False, help='output directory', default='plots')
+  parser.add_argument('-o', '--output', type=str, required=False, help='Name to put in output filenames', default='output')
+  parser.add_argument('--g-min', type=float, required=False, help='Minimum gluino mass', default=800)
+  parser.add_argument('--g-max', type=float, required=False, help='Maximum gluino mass', default=2500)
+  parser.add_argument('--l-min', type=float, required=False, help='Minimum LSP mass', default=0)
+  parser.add_argument('--l-max', type=float, required=False, help='Maximum LSP mass', default=1500)
+  parser.add_argument('--bin-size', type=float, required=False, help='Size of bins to use', default=100)
+  parser.add_argument('--x-dim', type=float, required=False, help='x-dimension of figure', default=800)
+  parser.add_argument('--y-dim', type=float, required=False, help='y-dimension of figure', default=600)
+  parser.add_argument('--top-mass', type=float, required=False, help='Mass of top quark [GeV]. Mainly meant to draw exclusion line.', default=173.34)
+  parser.add_argument('--do-run1', action='store_true', help='Add Run-1 line to graph')
+  parser.add_argument('--run1-color', type=int, required=False, help='Color of Run-1 line', default=46)
+  parser.add_argument('--run1-excl', type=str, required=False, help='CSV file containing Run-1 exclusion points', default='run1_limit.csv')
+  parser.add_argument('--run1-1sigma', type=str, required=False, help='CSV file containing Run-1 exclusion (+1 sigma) points', default='run1_limit_1sigma.csv')
+  parser.add_argument('-b', '--batch', dest='batch_mode', action='store_true', help='Enable batch mode for ROOT.')
+
+  # parse the arguments, throw errors if missing any
+  args = parser.parse_args()
+
   import ROOT
   ROOT.gROOT.SetBatch(args.batch_mode)
-
-  import utils
 
   from rootpy.plotting.style import set_style, get_style
   atlas = get_style('ATLAS')
