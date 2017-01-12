fc9d2bb282d4172f973b99ecff89a70d6eada30c
Giordon Stark <kratsg@gmail.com>
2017-01-12 15:15:04 -0600

    fix up paths

--- example_script.sh
+++ example_script.sh
@@ -16,10 +16,10 @@ python ../Optimization/optimize.py summary --searchDirectory significances/main 
 python ../Optimization/optimize.py hash summary_main.json --supercuts supercuts/main.json -o outputHash/main --use-summary
 
 # make the standard set of plots for significance, ratio, signal and background yields
-python ../Optimization/graph-grid.py --summary summary_main.json --lumi 35 -o main --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b
+python ../Optimization/graph-grid.py --summary summary_main.json --lumi 35 -o main --do-run1 --run1-excl run1_limit.csv --run1-1sigma run1_limit_1sigma.csv -b
 
 # make a set of plots showing the optimal cuts
-python ../Optimization/graph-cuts.py --summary summary_main.json --lumi 35 -o main --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b --outputHash outputHash/main --supercuts supercuts/main.json
+python ../Optimization/graph-cuts.py --summary summary_main.json --lumi 35 -o main --do-run1 --run1-excl run1_limit.csv --run1-1sigma run1_limit_1sigma.csv -b --outputHash outputHash/main --supercuts supercuts/main.json
 
 # compare with another summary you made
 python ../Optimization/summary-comparison.py --base-summary summary_old.json --comp-summary summary_main.json --lumi 35 -o main -b
