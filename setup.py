3b0f487b34d16cb08cf03f9e98fd2f6b7f11a3fd
Giordon Stark <kratsg@gmail.com>
2019-09-12 02:09:07 -0700

    Bump version: 0.7.2 → 0.8.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = read(
 
 setup(
     name="root_optimize",
-    version="0.7.2",
+    version="0.8.0",
     python_requires=">=3",
     description="Perform optimizations on flat ROOT TTrees",
     author="Giordon Stark",
